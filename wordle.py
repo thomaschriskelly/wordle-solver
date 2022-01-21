6ec3c7a576922decf2b953d5fdca358e3375e297
tom <tkelly@getresq.com>
2022-01-21 12:56:03 -0500

    Wordle Solver

---
+++ wordle.py
@@ -0,0 +1,35 @@
+from english_words import english_words_lower_alpha_set
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Solve Wordle')
+    parser.add_argument("whitelist", help="Whitelisted letters")
+    parser.add_argument("blacklist", help="Blacklisted letters")
+    parser.add_argument("positions", help="Known letter positions. eg C*AM*")
+    args = parser.parse_args()
+
+    five_letter_words = [
+        word for word in english_words_lower_alpha_set
+        if len(word) == 5
+    ]
+    whitelist = set([letter for letter in args.whitelist])
+    whitelisted = [
+        word for word in five_letter_words
+        if all(letter in word for letter in whitelist)
+    ]
+    blacklist = set([letter for letter in args.blacklist])
+    blacklisted = [
+        word for word in whitelisted
+        if not any(letter in word for letter in blacklist)
+    ]
+    position_filtered = [
+        word for word in blacklisted
+        if all(
+            letter == '*' or letter == word[index]
+            for index, letter in enumerate(args.positions)
+        )
+    ]
+    for word in position_filtered:
+        print(word)
+
+main()
