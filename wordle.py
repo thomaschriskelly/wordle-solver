a21213b33df887189e1ccd62e584a2f57d7f322e
tom <tkelly@getresq.com>
2022-02-18 15:27:46 -0500

    sort by letter frequency

--- wordle.py
+++ wordle.py
@@ -1,6 +1,42 @@
 from english_words import english_words_lower_alpha_set
 import argparse
 
+LETTERS_BY_FREQUENCY = [
+    'e',
+    't',
+    'a',
+    'o',
+    'i',
+    'n',
+    's',
+    'h',
+    'r',
+    'd',
+    'l',
+    'c',
+    'u',
+    'm',
+    'w',
+    'f',
+    'g',
+    'y',
+    'p',
+    'b',
+    'v',
+    'k',
+    'j',
+    'x',
+    'q',
+    'z',
+]
+
+def word_weight(word):
+    ''' lower is better '''
+    return sum(
+        LETTERS_BY_FREQUENCY.index(letter)
+        for letter in word
+    )
+
 def main():
     parser = argparse.ArgumentParser(description='Solve Wordle')
     parser.add_argument("whitelist", help="Whitelisted letters")
@@ -31,7 +67,14 @@ def main():
             for index, letter in enumerate(args.positions)
         )
     ]
-    for word in position_filtered:
+    sorted_words = sorted(
+        position_filtered,
+        key=word_weight,
+    )
+    CUTOFF = 20
+    for word in sorted_words[:CUTOFF]:
         print(word)
+    if(len(sorted_words) > CUTOFF):
+        print(f'+ {len(sorted_words) - CUTOFF} more')
 
 main()
